7306bfd94cf1537532e1def50f35938d3299ff9c
christianjensen2903 <cmj290300@gmail.com>
2022-09-13 08:34:23 +0200

    Clean data and add overlap feature

--- Pipeline.py
+++ Pipeline.py
@@ -1,5 +1,6 @@
 from typing import Callable
 from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
 
 
 class Pipeline():
@@ -16,6 +17,7 @@ class Pipeline():
         """Tokenize the dataset"""
         def add_tokenization(row):
             row['tokenized_question'] = tokenizer(row['question_text'])
+            row['tokenized_plaintext'] = tokenizer(row['document_plaintext'])
             return row
 
         self.train_data = self.train_data.map(add_tokenization)
@@ -24,7 +26,7 @@ class Pipeline():
     def label_answerable(self):
         """Label the dataset"""
         def add_label(row):
-            row['is_answerable'] = int(len(row["annotations"]['answer_start']) != 0)
+            row['is_answerable'] = row["annotations"]['answer_start'] != [-1]
             return row
         self.train_data = self.train_data.map(add_label)
         self.validation_data = self.validation_data.map(add_label)
@@ -36,7 +38,27 @@ class Pipeline():
                 tokenizer= lambda x: x,
                 preprocessor=lambda x: x,
         )
-        self.X = self.vectorizer.fit_transform(self.train_data['tokenized_question'])
+        def add_features(row):
+            row['overlap'] = len([e for e in row['tokenized_question'] if e in row['tokenized_plaintext']])
+            return row
+
+        self.train_data = self.train_data.map(add_features)
+        self.validation_data = self.validation_data.map(add_features)
+
+        # self.X = self.vectorizer.fit_transform(self.train_data['tokenized_question'])
+        self.X = np.array(self.train_data['overlap']).reshape(-1, 1)
+        self.X_validation = np.array(self.validation_data['overlap']).reshape(-1, 1)
+
+
+    def clean(self, cleaner):
+        """Clean the dataset"""
+        def clean_row(row):
+            row['cleaned_question'] = cleaner(row['tokenized_question'])
+            row['cleaned_plaintext'] = cleaner(row['tokenized_plaintext'])
+            return row
+
+        self.train_data = self.train_data.map(clean_row)
+        self.validation_data = self.validation_data.map(clean_row)
 
 
     def train(self, model):
@@ -48,7 +70,7 @@ class Pipeline():
     
     def validate(self):
         """Validate the model"""
-        self.X_validation = self.vectorizer.transform(self.validation_data['tokenized_question'])
+        # self.X_validation = self.vectorizer.transform(self.validation_data['tokenized_question'])
         self.Y_validation = self.validation_data['is_answerable']
         print(f'Validation accuracy: {self.model.score(self.X_validation, self.Y_validation)}')
         

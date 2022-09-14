258e4c9b8fff55772fe7414f446e56d6331f19b7
christianjensen2903 <cmj290300@gmail.com>
2022-09-14 08:38:54 +0200

    Model class

---
+++ Model.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Model(ABC):
+
+    def train(self):
+        """Train the model"""
+        pass
+
+    def predict(self, X):
+        """Predict the answer"""
+        pass
+
+    def evaluate(self):
+        """Evaluate the model"""
+        pass
+
+    def save(self):
+        """Save the model"""
+        pass
+
+    def load(self):
+        """Load the model"""
+        pass

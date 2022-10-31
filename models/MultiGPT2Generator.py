e09fe9d6db6bdf587dd05c58e39303fba5561d4d
hojmax <axelhojmark@gmail.com>
2022-10-31 19:28:43 +0100

    Increasing batch size

--- models/MultiGPT2Generator.py
+++ models/MultiGPT2Generator.py
@@ -34,8 +34,8 @@ class MultiGPT2Generator(Model, feature_extraction):
         self.training_args = TrainingArguments(
             output_dir=self.get_save_path(),
             num_train_epochs=3,
-            per_device_train_batch_size=1,
-            per_device_eval_batch_size=1,
+            per_device_train_batch_size=16,
+            per_device_eval_batch_size=16,
             warmup_steps=200,
             weight_decay=0.01,
             prediction_loss_only=True,

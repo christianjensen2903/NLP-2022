93ac72ff0477224692f6ab5a804c3c6faac4c037
hojmax <axelhojmark@gmail.com>
2022-10-31 19:45:55 +0100

    Higher batch

--- models/MultiGPT2Generator.py
+++ models/MultiGPT2Generator.py
@@ -34,8 +34,8 @@ class MultiGPT2Generator(Model, feature_extraction):
         self.training_args = TrainingArguments(
             output_dir=self.get_save_path(),
             num_train_epochs=3,
-            per_device_train_batch_size=4,
-            per_device_eval_batch_size=4,
+            per_device_train_batch_size=8,
+            per_device_eval_batch_size=8,
             warmup_steps=200,
             weight_decay=0.01,
             prediction_loss_only=True,

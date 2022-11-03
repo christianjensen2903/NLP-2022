4256d4e1d45dbf9063de1803f4e0698c8ced1714
cringgaard <christoffer@ringgaard.com>
2022-11-03 16:49:50 +0100

    Added explainability to RF and updated adverserial

--- adverserial.py
+++ adverserial.py
@@ -1,8 +1,10 @@
-from models.Logistic.BOWLogistic import BOWLogistic
 from Preprocess import Preprocess
 from Pipeline import Pipeline
 from languages.English import English
 import pandas as pd
+from models.Logistic.CBOW_BOWLogistic import CBOW_BOWLogistic
+from models.Model import Model
+from models.RandomForest.CBOW_BOWRandomForest import CBOW_BOWRandomForest
 # import nltk
 # nltk.download('punkt')
 # nltk.download('stopwords')
@@ -16,29 +18,31 @@ pipeline = Pipeline()
 preprocessor = Preprocess(language.tokenize, language.clean)
 preprocessor.from_datasets = False
 raw_data = {
-  "question_text"      : ["When was Queen Elizabeth II born?" , "What is the name for a male horse?"],
+  "question_text"      : ["When was Queen Elizabeth II born?" , "What is the name for a male horse?" , "What is the name for a male horse?"],
   "document_plaintext" : [
     "Queen Elizabeth II was the queen of England from 1952 until she died on the 8th of september 2022." ,
-    "A horse is frequently referred to as a stallion once he fathers a foal. While in most of the western world stallions are primarily kept for breeding, it is popular in parts of the Middle East and Asia for stallions to be used for riding (almost always by men)." ]
+    "A horse is frequently referred to as a stallion once he fathers a foal. While in most of the western world stallions are primarily kept for breeding, it is popular in parts of the Middle East and Asia for stallions to be used for riding (almost always by men)." ,
+    "A mare is an adult female horse or other equine. In most cases, a mare is a female horse over the age of three, and a filly is a female horse three and younger. In Thoroughbred horse racing, a mare is defined as a female horse more than four years old. The word can also be used for other female equine animals, particularly mules and zebras, but a female donkey is usually called a 'jenny'. A broodmare is a mare used for breeding. A horse's female parent is known as its dam."]
 }
 dataset = pd.DataFrame.from_dict(data = raw_data)
 data = preprocessor.preprocess(dataset)
 
 
-model = BOWLogistic()
-model.set_language(language.name)
-model.load()
+models :list[Model] =[
+  CBOW_BOWLogistic(),
+  CBOW_BOWRandomForest()
+]
+for model in models:
+  model.set_language(language.name)
+  model.load()
 
+  X_validation = model.extract_X(data)
+  y_validation = [0,1,0]
 
-print(data)
+  pipeline.evaluate(
+              model,
+              X_validation,
+              y_validation
+          )
 
-X_validation = model.extract_X(data)
-y_validation = [0,1]
-
-pipeline.evaluate(
-            model,
-            X_validation,
-            y_validation
-        )
-
-print(model.predict(X_validation))
+  print(model.predict(X_validation))
--- models/RandomForest/RandomForest.py
+++ models/RandomForest/RandomForest.py
@@ -1,11 +1,14 @@
 from models.Model import Model
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
 from pickle import dump, load
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 class RandomForest(Model):
-    def __init__(self, language):
-        super().__init__(language)
+    def __init__(self):
+        super().__init__()
         self.question_vectorizer = None
         self.plaintext_vectorizer = None
         self.first_word_vectorizer = None
@@ -29,7 +32,36 @@ class RandomForest(Model):
             open(self.get_save_path('pkl'), 'rb'))
 
     def weights(self):
-        pass
-    
-    def explainability(self, n=5):
-        pass
+        return dict(zip(
+            [f'*QUESTION* {x}' for x in self.question_vectorizer.vocabulary_] +
+            [f'*PLAINTEXT* {x}' for x in self.plaintext_vectorizer.vocabulary_] +
+            [f'*FIRSTWORD* {x}' for x in self.first_word_vectorizer.vocabulary_] +
+            ['*OVERLAP*'] +
+            [f'*QUESTION_CONTINOUS* {x}' for x in range(100)] +
+            [f'*PLAINTEXT_CONTINOUS* {x}' for x in range(100)] +
+            ['*EUCLIDEAN*'] +
+            ['*COSINE*'] +
+            ['*BERT_SCORE*'],
+            list(self.model.feature_importances_),
+        ))
+
+
+    def explainability(self , X , y , n = 10):
+        most_important = sorted(self.weights().items(), key=lambda item: abs(item[1]), reverse=True)[:n]
+        names , values = list(zip(*most_important))
+
+        plot_df = pd.DataFrame()
+        plot_df['names'] = names
+        plot_df['values'] = values
+        plot_df.plot( x = 'names' , y = 'values', kind='bar' , figsize=(20,10))
+        plt.show()
+
+        print (
+            "EXPLAINABILITY:\n",
+            "Top {} most important features:\n".format(n),
+            sorted(self.weights().items(), key=lambda item: item[1], reverse=True)[:n], # n most important
+            "\n\n",
+            "Top {} least important features:\n".format(n),
+            sorted(self.weights().items(), key=lambda item: item[1], reverse=False)[:n] # n least important
+            )
+        print(f'Confusion_matrix Matrix:', confusion_matrix(y , self.model.predict(X) , normalize = "all"))

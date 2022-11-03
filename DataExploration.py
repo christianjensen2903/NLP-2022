dc090adc724fbc9738972d42ff56965eafb00b03
hojmax <axelhojmark@gmail.com>
2022-11-03 09:48:42 +0100

    Add frequency to data exploration

--- DataExploration.py
+++ DataExploration.py
@@ -17,9 +17,10 @@ class DataExploration():
     
     def find_frequent_words(self):
         """Find the most common first and last words"""
+        sentence_count = len(self.data)
         count_words = lambda text: np.unique(text, return_counts=True) # Count occurences of words in text
         sort_words = lambda word_count: np.argsort(word_count[1])[::-1] # Get list of sorted indices based on most frequent words
-        zip_words = lambda word_counts, sort_indices: list(zip(word_counts[0][sort_indices],word_counts[1][sort_indices])) # Zip the most frequent words with its number of occurences
+        zip_words = lambda word_counts, sort_indices: list(zip(word_counts[0][sort_indices],word_counts[1][sort_indices],word_counts[1][sort_indices] / sentence_count)) # Zip the most frequent words with its number of occurences
         def find_most_common(text):
             """Finds the most frequent words in a text together with its number of occurences"""
             word_count = count_words(text)
